92b2dc8c01d6b9299b72c39178301bda96f22781
zizoubrown <zizumoha2015@gmail.com>
2020-10-23 22:40:54 +0300

    register message model

--- ig/admin.py
+++ ig/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Profile, Comment
+from .models import Profile, Comment, Message
+
+admin.site.site_header = "Instagram Admin"
+admin.site.site_title = "Instagram Admin Area"
+admin.site.index_title = "Welcome to instagram admin"
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Comment)
+admin.site.register(Message)

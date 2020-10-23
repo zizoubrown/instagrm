cce34ad3924be7bb3d666fc083cf46ceadeba47f
zizoubrown <zizumoha2015@gmail.com>
2020-10-24 00:34:15 +0300

    add template path

--- insta/settings.py
+++ insta/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'insta.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'insta/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

55fe8a3bf6c0b7d7130e7524f1707a94b0ef8b84
zizoubrown <zizumoha2015@gmail.com>
2020-10-23 18:00:56 +0300

    add edit profile class form

--- ig/forms.py
+++ ig/forms.py
@@ -52,3 +52,15 @@ class ChangePasswordForm(forms.ModelForm):
 		if new_password != confirm_password:
 			self._errors['new_password'] =self.error_class(['Passwords do not match.'])
 		return self.cleaned_data
+
+class EditProfileForm(forms.ModelForm):
+	picture = forms.ImageField(required=False)
+	first_name = forms.CharField(widget=forms.TextInput(), max_length=50, required=False)
+	last_name = forms.CharField(widget=forms.TextInput(), max_length=50, required=False)
+	location = forms.CharField(widget=forms.TextInput(), max_length=25, required=False)
+	url = forms.URLField(widget=forms.TextInput(), max_length=60, required=False)
+	profile_info = forms.CharField(widget=forms.TextInput(), max_length=260, required=False)
+
+	class Meta:
+		model = Profile
+		fields = ('picture', 'first_name', 'last_name', 'location', 'url', 'profile_info')

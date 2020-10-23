787325ae473c355ceabbf2184131f678d2bbc137
zizoubrown <zizumoha2015@gmail.com>
2020-10-23 17:27:28 +0300

    add timeline view function

--- post/views.py
+++ post/views.py
@@ -14,3 +14,33 @@ from django.urls import reverse
 #from authy.models import Profile
 
 # Create your views here.
+@login_required
+def index(request):
+	posts = Post.objects.all()
+	template = loader.get_template('index.html')
+	context = {'post_items': posts}
+
+	return HttpResponse(template.render(context, request))
+
+@login_required
+def timeline(request):
+	user = request.user
+	posts = Stream.objects.filter(user=user)
+	users = User.objects.all()
+
+	group_ids = []
+
+	for post in posts:
+		group_ids.append(post.post_id)
+		
+	post_items = Post.objects.filter(id__in=group_ids).all().order_by('-posted')		
+
+	template = loader.get_template('timeline.html')
+
+	context = {
+	'post_items': post_items,
+	'users':users,
+
+	}
+
+	return HttpResponse(template.render(context, request))

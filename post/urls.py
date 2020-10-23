dde005766f2324c0ada6b6111777ee015fcc8099
zizoubrown <zizumoha2015@gmail.com>
2020-10-23 17:39:38 +0300

    add path for the view functions created

---
+++ post/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from post.views import index, NewPost, PostDetails, tags, like, favorite, timeline
+
+urlpatterns = [
+   	path('', index, name='index'),
+   	path('timeline/', timeline, name='timeline'),
+   	path('newpost/', NewPost, name='newpost'),
+   	#path('<uuid:post_id>', PostDetails, name='postdetails'),
+   	path('<uuid:post_id>/like', like, name='postlike'),
+   	path('<uuid:post_id>/favorite', favorite, name='postfavorite'),
+   	path('tag/<slug:tag_slug>', tags, name='tags'),
+]

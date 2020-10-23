3f9f4781c838066d7576d8ed008528eb919ae90d
zizoubrown <zizumoha2015@gmail.com>
2020-10-23 16:44:02 +0300

    add apps in the settings

--- insta/settings.py
+++ insta/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ig',
+    'post',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [

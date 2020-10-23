aa82be1eab5ecef4182d2ef19c98a21d47fa3705
zizoubrown <zizumoha2015@gmail.com>
2020-10-23 18:02:35 +0300

    modify forms

--- ig/forms.py
+++ ig/forms.py
@@ -3,6 +3,24 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from .models import Profile
 
+def ForbiddenUsers(value):
+	forbidden_users = ['admin', 'css', 'js', 'authenticate', 'login', 'logout', 'administrator', 'root',
+	'email', 'user', 'join', 'sql', 'static', 'python', 'delete']
+	if value.lower() in forbidden_users:
+		raise ValidationError('Invalid name for user, this is a reserverd word.')
+
+def InvalidUser(value):
+	if '@' in value or '+' in value or '-' in value:
+		raise ValidationError('This is an Invalid user, Do not user these chars: @ , - , + ')
+
+def UniqueEmail(value):
+	if User.objects.filter(email__iexact=value).exists():
+		raise ValidationError('User with this email already exists.')
+
+def UniqueUser(value):
+	if User.objects.filter(username__iexact=value).exists():
+		raise ValidationError('User with this username already exists.')
+
 class SignupForm(forms.ModelForm):
 	username = forms.CharField(widget=forms.TextInput(), max_length=30, required=True,)
 	email = forms.CharField(widget=forms.EmailInput(), max_length=100, required=True,)

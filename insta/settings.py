c240b41b73c7779aa1915907aa1357ad044768f9
zizoubrown <zizumoha2015@gmail.com>
2020-10-23 23:36:30 +0300

    add database

--- insta/settings.py
+++ insta/settings.py
@@ -78,8 +78,9 @@ WSGI_APPLICATION = 'insta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'igram',
+        'USER': 'moringa'
     }
 }
 

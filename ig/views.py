0e1ad52682e9be7bc88f3afa32fc4746fe8b7663
zizoubrown <zizumoha2015@gmail.com>
2020-10-23 18:10:45 +0300

    add signup view function

--- ig/views.py
+++ ig/views.py
@@ -94,3 +94,22 @@ def login(request):
 		'form':form,
 	}
     return render(request, 'login.html',context)
+
+def Signup(request):
+	if request.method == 'POST':
+		form = SignupForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			email = form.cleaned_data.get('email')
+			password = form.cleaned_data.get('password')
+			User.objects.create_user(username=username, email=email, password=password)
+			return redirect('login')
+	else:
+		form = SignupForm()
+	
+	context = {
+		'form':form,
+	}
+
+	return render(request, 'signup.html', context)
+

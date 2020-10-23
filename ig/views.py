a90a447adf8dd595c653b53b30a382b9c57b071f
zizoubrown <zizumoha2015@gmail.com>
2020-10-23 22:29:54 +0300

    add usersearch view function

--- ig/views.py
+++ ig/views.py
@@ -212,3 +212,25 @@ def Inbox(request):
 	template = loader.get_template('direct/direct.html')
 
 	return HttpResponse(template.render(context, request))
+
+@login_required
+def UserSearch(request):
+	query = request.GET.get("q")
+	context = {}
+	
+	if query:
+		users = User.objects.filter(Q(username__icontains=query))
+
+		#Pagination
+		paginator = Paginator(users, 6)
+		page_number = request.GET.get('page')
+		users_paginator = paginator.get_page(page_number)
+
+		context = {
+				'users': users_paginator,
+			}
+	
+	template = loader.get_template('direct/search_user.html')
+	
+	return HttpResponse(template.render(context, request))
+

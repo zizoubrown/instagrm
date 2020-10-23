8f23f92cb15142e3b368702aede8c6579aba2d4d
zizoubrown <zizumoha2015@gmail.com>
2020-10-23 18:10:09 +0300

    add login view function

--- ig/views.py
+++ ig/views.py
@@ -83,3 +83,14 @@ def UserProfileFavorites(request, username):
 
 	return HttpResponse(template.render(context, request))
 
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect(request,'/')
+    context = {
+		'form':form,
+	}
+    return render(request, 'login.html',context)

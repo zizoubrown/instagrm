99fced05ece4e1974f98be1d9dcbbae1e472743f
zizoubrown <zizumoha2015@gmail.com>
2020-10-23 18:25:49 +0300

    add comment model

--- ig/models.py
+++ ig/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from post.models import Post
 
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 
 from PIL import Image
 from django.conf import settings
@@ -51,3 +51,29 @@ def save_user_profile(sender, instance, **kwargs):
 
 post_save.connect(create_user_profile, sender=User)
 post_save.connect(save_user_profile, sender=User)
+
+class Comment(models.Model):
+	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	body = models.TextField()
+	date = models.DateTimeField(auto_now_add=True)
+
+	def user_comment_post(sender, instance, *args, **kwargs):
+		comment = instance
+		post = comment.post
+		text_preview = comment.body[:90]
+		sender = comment.user
+		notify = Notification(post=post, sender=sender, user=post.user, text_preview=text_preview ,notification_type=2)
+		notify.save()
+
+	def user_del_comment_post(sender, instance, *args, **kwargs):
+		like = instance
+		post = like.post
+		sender = like.user
+
+		notify = Notification.objects.filter(post=post, sender=sender, notification_type=2)
+		notify.delete()
+
+#Comment
+post_save.connect(Comment.user_comment_post, sender=Comment)
+post_delete.connect(Comment.user_del_comment_post, sender=Comment)

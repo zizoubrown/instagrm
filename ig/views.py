307836199dee736f7a64b6b8467c75447ecc3485
zizoubrown <zizumoha2015@gmail.com>
2020-10-23 18:11:42 +0300

    add change password view function

--- ig/views.py
+++ ig/views.py
@@ -113,3 +113,25 @@ def Signup(request):
 
 	return render(request, 'signup.html', context)
 
+@login_required
+def PasswordChange(request):
+	user = request.user
+	if request.method == 'POST':
+		form = ChangePasswordForm(request.POST)
+		if form.is_valid():
+			new_password = form.cleaned_data.get('new_password')
+			user.set_password(new_password)
+			user.save()
+			update_session_auth_hash(request, user)
+			return redirect('change_password_done')
+	else:
+		form = ChangePasswordForm(instance=user)
+
+	context = {
+		'form':form,
+	}
+
+	return render(request, 'change_password.html', context)
+
+def PasswordChangeDone(request):
+	return render(request, 'change_password_done.html')

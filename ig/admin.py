8743806c129406fc6d9c9704543a34de33666b42
zizoubrown <zizumoha2015@gmail.com>
2020-10-24 08:27:15 +0300

    register notification model

--- ig/admin.py
+++ ig/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile, Comment, Message
+from .models import Profile, Comment, Message, Notification
 
 admin.site.site_header = "Instagram Admin"
 admin.site.site_title = "Instagram Admin Area"
@@ -9,3 +9,4 @@ admin.site.index_title = "Welcome to instagram admin"
 admin.site.register(Profile)
 admin.site.register(Comment)
 admin.site.register(Message)
+admin.site.register(Notification)
